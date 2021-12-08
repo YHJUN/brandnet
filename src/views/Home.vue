25194de89ebec8d8390c924713b5f71ae3b1aa7d
yhj <1151905584@163.com>
2021-12-08 14:18:22 +0800

    二次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <van-button type="primary">主要按钮</van-button>
-<van-button type="success">成功按钮</van-button>
-<van-button type="default">默认按钮</van-button>
-<van-button type="warning">警告按钮</van-button>
-<van-button type="danger">危险按钮</van-button>
+
   </div>
 </template>
 
@@ -20,5 +17,5 @@ export default {
     HelloWorld,
     [Button.name]:Button
   }
-}
+} 
 </script>
